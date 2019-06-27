593a0cdb407f606f56c27cd314b3fb5ea5474c0f
donghyunlee <donghyun3363@gmail.com>
2019-06-27 16:06:37 +0900

    add

--- src/main/java/com/dong/springboot/controller/WebController.java
+++ src/main/java/com/dong/springboot/controller/WebController.java
@@ -1,7 +1,11 @@
 package com.dong.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.dong.springboot.service.PostsService;
 
 import lombok.AllArgsConstructor;
 
@@ -9,8 +13,24 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class WebController {
 
-    @GetMapping("/")
-    public String main() {
-        return "main";
-    }
+	private PostsService postsService;
+
+	@GetMapping("/")
+	public String main(Model model) {
+		model.addAttribute("posts", postsService.findAllDesc());
+		System.out.println("model: " + model + ", " + model.toString());
+		return "main";
+	}
+
+	@ResponseBody
+	@GetMapping("/test")
+	public String test(Model model) {
+
+		String values;
+
+		values = "{\"1\":\"saaaaa\", \"2\", \"bbbbb\"}";
+
+		return values;
+	}
+
 }
--- src/main/java/com/dong/springboot/service/PostsService.java
+++ src/main/java/com/dong/springboot/service/PostsService.java
@@ -1,9 +1,13 @@
 package com.dong.springboot.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dong.springboot.domain.posts.PostsRepository;
+import com.dong.springboot.dto.PostsMainResponseDto;
 import com.dong.springboot.dto.PostsSaveRequestDto;
 
 import lombok.AllArgsConstructor;
@@ -18,4 +22,13 @@ public class PostsService {
 	public Long save(PostsSaveRequestDto dto) {
 		return postsRepository.save(dto.toEntity()).getId();
 	}
+	
+	@Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+		
+		List<PostsMainResponseDto> list = postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
+        return list;
+    }
 }
